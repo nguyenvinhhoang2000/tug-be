ebba717e5948b3937df74d7fdb81a4c842022825
Hoàng Nguyễn Vĩnh <nguyenvinhhoang2000@gmail.com>
2023-03-08 19:41:17 +0700

    update

--- index.js
+++ index.js
@@ -14,12 +14,11 @@ app.listen(5001, () => {
 })
 
 app.get('/', (req, res) => {
-    res.send("tug-be")
+    res.send("tug-be-update")
 })
 
 // init web3
 const web3 = new Web3(new Web3.providers.HttpProvider('https://rpc-testnet.onuschain.io'));
-const currentTime = new Date()
 
 const ContractTugPair1 = new web3.eth.Contract(constants.TUGPAIR_ABI, constants.TUGPAIR_SC_1)
 const ContractTugPair2 = new web3.eth.Contract(constants.TUGPAIR_ABI, constants.TUGPAIR_SC_2)
@@ -32,14 +31,15 @@ const main = async () => {
     const resEpochDuration2 = await ContractTugPair2.methods.epochDuration().call()
 
     const getCurrentEpochTime1 = async () => {
+        const currentTime = new Date()
         const resStartTime1 = await ContractTugPair1.methods.startTime().call()
         const resLastestEpoch1 = await ContractTugPair1.methods.getLatestEpoch().call()
 
         const currentEpochTime1 = Number(resStartTime1) + (Number(resLastestEpoch1) * Number(resEpochDuration1))
         
         if (currentEpochTime1 < currentTime.getTime() / 1000) {
-            var resEpochData1 = await ContractTugPair1.methods.epochData(resLastestEpoch1).call()
-    
+            const resEpochData1 = await ContractTugPair1.methods.epochData(resLastestEpoch1).call()
+            
             if (resEpochData1.token0SharesIssued > 0 || resEpochData1.token1SharesIssued > 0) {
                 try {
                     const tx = ContractTugPair1.methods.updateEpoch()
@@ -61,23 +61,23 @@ const main = async () => {
                     )
                     
                     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-                    console.log("🚀 ~ file: index.js:47 ~ main ~ receipt:", receipt)
                     
                 } catch (error) {
-                    console.log(error);
+                    console.log('========1', error);
                 }
             }
         }
     }
 
     const getCurrentEpochTime2 = async () => {
+        const currentTime = new Date()
         const resStartTime2 = await ContractTugPair2.methods.startTime().call()
         const resLastestEpoch2 = await ContractTugPair2.methods.getLatestEpoch().call()
 
         const currentEpochTime2 = Number(resStartTime2) + (Number(resLastestEpoch2) * Number(resEpochDuration2))
        
         if (currentEpochTime2 < currentTime.getTime() / 1000) {
-            var resEpochData2 = await ContractTugPair2.methods.epochData(resLastestEpoch2).call()
+            const resEpochData2 = await ContractTugPair2.methods.epochData(resLastestEpoch2).call()
     
             if (resEpochData2.token0SharesIssued > 0 || resEpochData2.token1SharesIssued > 0) {
                 try {
@@ -96,21 +96,20 @@ const main = async () => {
                         nonce,
                         chainId: networkId,
                     },
-                    process.env.PRIVATE_KEY,
+                    '8102957e1c2858fe3661ae1d0b1eaecc2b3f85885613eb99d6dd175998806afc',
                     )
                     
                     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-                    console.log("🚀 ~ file: index.js:47 ~ main ~ receipt:", receipt)
                     
                 } catch (error) {
-                    console.log(error);
+                    console.log('========2', error);
                 }
             }
         }
     }
 
-    setInterval(getCurrentEpochTime1, resEpochDuration1);
-    setInterval(getCurrentEpochTime2, resEpochDuration2);
+    setInterval(getCurrentEpochTime1, Number(resEpochDuration1) * 1000);
+    setInterval(getCurrentEpochTime2, Number(resEpochDuration2) * 1000);
 }
 
 main()
